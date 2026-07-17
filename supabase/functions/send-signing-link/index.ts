a7d2712e1ae40d2c38cf5fe71775595e39aad7f0
Luis <obstipate@gmail.com>
2026-07-16 20:24:39 -0500

    Embed the standard email signature in signing-link and signed-doc emails
    These two flows were missing the HTML_SIGNATURE block already used by
    send-manu-email and send-sourcing-email, so they fell back to a bare
    "Best regards, SIINGE STUDIO" line with no name/photo/contact info.

--- supabase/functions/send-signing-link/index.ts
+++ supabase/functions/send-signing-link/index.ts
@@ -7,6 +7,56 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
+const HTML_SIGNATURE = `<table cellpadding="0" cellspacing="0" style="border-collapse: collapse; line-height: 1.15; width: 100%;" id="isPasted" width="100%">
+	<tbody valign="middle">
+		<tr valign="inherit">
+			<td style="vertical-align:middle;padding:.01px 12px 0.01px 1px;width:92px;text-align:center;" valign="middle" align="center"><img border="0" src="https://permanent-assets-download.flockmail.com/signature/2408373/2024-06-03_36c3cd811224bc3a55b5_55761" width="92" alt="photo" style="width: 78px; vertical-align: middle; border-radius: 0px; height: 83px; border: 0px; display: block;"></td>
+			<td valign="top" style="padding:.01px 0.01px 0.01px 12px;vertical-align:top;border-left:solid 1px #BDBDBD;"><strong><strong><br></strong></strong>
+
+				<table cellpadding="0" cellspacing="0" style="border-collapse: collapse; width: 100%;" width="100%">
+					<tbody valign="middle">
+						<tr valign="inherit">
+							<td style="padding:.01px;" valign="inherit">
+
+								<p style="margin:.1px;line-height:108.0%;font-size:16px;"><span style="font-size: 12pt;"><strong><strong>Luis Dom&iacute;nguez</strong><br style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; user-select: inherit; scrollbar-color: auto; box-sizing: border-box;"></strong></span><span style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 11pt; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; float: none; display: inline !important;"><strong>Product Operations Manager</strong></span></p>
+
+								<p style="margin:.1px;line-height:108.0%;font-size:16px;"><span style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 11pt; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; float: none; display: inline !important;"><strong>​</strong></span>
+									<br style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; user-select: inherit; scrollbar-color: auto; box-sizing: border-box;"><span style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">+57 350 201 4528 &nbsp; | &nbsp;&nbsp;</span><a href="https://www.siinge.studio/" title="https://www.siinge.studio" style="color: rgb(76, 140, 246); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background: rgba(0, 0, 0, 0); user-select: auto; scrollbar-color: auto; box-sizing: border-box;" target="_blank">www.siinge.studio</a>
+									<br style="color: rgb(51, 51, 51); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; user-select: inherit; scrollbar-color: auto; box-sizing: border-box;"><a href="mailto:production@siinge.studio" title="mailto:production@siinge.studio" style="color: rgb(76, 140, 246); font-family: Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background: rgba(0, 0, 0, 0); user-select: auto; scrollbar-color: auto; box-sizing: border-box;" target="_blank">production@siinge.studio</a></p>
+							</td>
+						</tr>
+						<tr valign="inherit">
+							<td valign="inherit">
+
+								<table cellpadding="0" cellspacing="0" style="border-collapse: collapse; width: 100%;" width="100%">
+									<tbody valign="middle">
+										<tr valign="inherit">
+											<td style="padding-top:6px;white-space:nowrap;width:138px;font-family:Arial;" valign="inherit"><span style="font-size: 11pt;">​</span>
+												<br>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<table cellpadding="0" cellspacing="0" width="100%" style="width:100%;color:gray;border-top:1px solid gray;line-height:normal;">
+	<tbody valign="middle">
+		<tr valign="inherit">
+			<td style="padding:9px 8px 0 0;" valign="inherit">
+
+				<p style="color:#888888;text-align:left;font-size:10px;margin:1px;line-height:120%;font-family:Arial ;">IMPORTANT: The contents of this email and any attachments are confidential. They are intended for the named recipient(s) only. If you have received this email by mistake, please notify the sender immediately and do not disclose the contents to anyone or make copies thereof.</p>
+			</td>
+		</tr>
+	</tbody>
+</table>`
+
 serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders })
@@ -80,6 +130,7 @@ serve(async (req: Request) => {
         <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
           ${custom_body.replace(/\n/g, '<br>')}
         </div>
+        ${HTML_SIGNATURE}
         ${pixelTag}
       `
     } else {
@@ -94,12 +145,8 @@ serve(async (req: Request) => {
           ${signButton}
 
           <p>If you have any questions, please reach out to us.</p>
-
-          <p>Best regards,<br>
-          <strong>SIINGE STUDIO</strong><br>
-          <a href="https://www.siinge.studio" style="color: #6366f1;">www.siinge.studio</a>
-          </p>
         </div>
+        ${HTML_SIGNATURE}
         ${pixelTag}
       `
     }
